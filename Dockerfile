04b0f107c1efcb1574f8a4384e02ed82023412d8
hazel <hazel.shen@linecorp.com>
2019-06-02 20:24:09 +0800

    🚚Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM alpine:3.2
-RUN apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
-ADD my-plugin /bin/
-ENTRYPOINT ["/bin/my-plugin"]
+FROM golang:alpine
+WORKDIR /
+ADD . /
+RUN apk add --no-cache git mercurial \
+    && go get github.com/tidwall/sjson \
+    && apk del git mercurial
+RUN cd / && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main
+ENTRYPOINT ["/main"]
--- main.go
+++ main.go
@@ -1,242 +1,75 @@
 package main
 
 import (
+    "os"
 	"fmt"
-	"os"
-
-	"github.com/codegangsta/cli"
-	_ "github.com/joho/godotenv/autoload"
+	"io/ioutil"
+	"log"
+    "net/http"
+    "encoding/base64"
+    "github.com/tidwall/sjson"
+    s "strings"
 )
-
-var version string // build number set at compile-time
-
-func main() {
-	app := cli.NewApp()
-	app.Name = "my plugin"
-	app.Usage = "my plugin usage"
-	app.Action = run
-	app.Version = version
-	app.Flags = []cli.Flag{
-
-		//
-		// repo args
-		//
-
-		cli.StringFlag{
-			Name:   "repo.fullname",
-			Usage:  "repository full name",
-			EnvVar: "DRONE_REPO",
-		},
-		cli.StringFlag{
-			Name:   "repo.owner",
-			Usage:  "repository owner",
-			EnvVar: "DRONE_REPO_OWNER",
-		},
-		cli.StringFlag{
-			Name:   "repo.name",
-			Usage:  "repository name",
-			EnvVar: "DRONE_REPO_NAME",
-		},
-		cli.StringFlag{
-			Name:   "repo.link",
-			Usage:  "repository link",
-			EnvVar: "DRONE_REPO_LINK",
-		},
-		cli.StringFlag{
-			Name:   "repo.avatar",
-			Usage:  "repository avatar",
-			EnvVar: "DRONE_REPO_AVATAR",
-		},
-		cli.StringFlag{
-			Name:   "repo.branch",
-			Usage:  "repository default branch",
-			EnvVar: "DRONE_REPO_BRANCH",
-		},
-		cli.BoolFlag{
-			Name:   "repo.private",
-			Usage:  "repository is private",
-			EnvVar: "DRONE_REPO_PRIVATE",
-		},
-		cli.BoolFlag{
-			Name:   "repo.trusted",
-			Usage:  "repository is trusted",
-			EnvVar: "DRONE_REPO_TRUSTED",
-		},
-
-		//
-		// commit args
-		//
-
-		cli.StringFlag{
-			Name:   "remote.url",
-			Usage:  "git remote url",
-			EnvVar: "DRONE_REMOTE_URL",
-		},
-		cli.StringFlag{
-			Name:   "commit.sha",
-			Usage:  "git commit sha",
-			EnvVar: "DRONE_COMMIT_SHA",
-		},
-		cli.StringFlag{
-			Name:   "commit.ref",
-			Value:  "refs/heads/master",
-			Usage:  "git commit ref",
-			EnvVar: "DRONE_COMMIT_REF",
-		},
-		cli.StringFlag{
-			Name:   "commit.branch",
-			Value:  "master",
-			Usage:  "git commit branch",
-			EnvVar: "DRONE_COMMIT_BRANCH",
-		},
-		cli.StringFlag{
-			Name:   "commit.message",
-			Usage:  "git commit message",
-			EnvVar: "DRONE_COMMIT_MESSAGE",
-		},
-		cli.StringFlag{
-			Name:   "commit.link",
-			Usage:  "git commit link",
-			EnvVar: "DRONE_COMMIT_LINK",
-		},
-		cli.StringFlag{
-			Name:   "commit.author.name",
-			Usage:  "git author name",
-			EnvVar: "DRONE_COMMIT_AUTHOR",
-		},
-		cli.StringFlag{
-			Name:   "commit.author.email",
-			Usage:  "git author email",
-			EnvVar: "DRONE_COMMIT_AUTHOR_EMAIL",
-		},
-		cli.StringFlag{
-			Name:   "commit.author.avatar",
-			Usage:  "git author avatar",
-			EnvVar: "DRONE_COMMIT_AUTHOR_AVATAR",
-		},
-
-		//
-		// build args
-		//
-
-		cli.StringFlag{
-			Name:   "build.event",
-			Value:  "push",
-			Usage:  "build event",
-			EnvVar: "DRONE_BUILD_EVENT",
-		},
-		cli.IntFlag{
-			Name:   "build.number",
-			Usage:  "build number",
-			EnvVar: "DRONE_BUILD_NUMBER",
-		},
-		cli.IntFlag{
-			Name:   "build.created",
-			Usage:  "build created",
-			EnvVar: "DRONE_BUILD_CREATED",
-		},
-		cli.IntFlag{
-			Name:   "build.started",
-			Usage:  "build started",
-			EnvVar: "DRONE_BUILD_STARTED",
-		},
-		cli.IntFlag{
-			Name:   "build.finished",
-			Usage:  "build finished",
-			EnvVar: "DRONE_BUILD_FINISHED",
-		},
-		cli.StringFlag{
-			Name:   "build.status",
-			Usage:  "build status",
-			Value:  "success",
-			EnvVar: "DRONE_BUILD_STATUS",
-		},
-		cli.StringFlag{
-			Name:   "build.link",
-			Usage:  "build link",
-			EnvVar: "DRONE_BUILD_LINK",
-		},
-		cli.StringFlag{
-			Name:   "build.deploy",
-			Usage:  "build deployment target",
-			EnvVar: "DRONE_DEPLOY_TO",
-		},
-		cli.BoolFlag{
-			Name:   "yaml.verified",
-			Usage:  "build yaml is verified",
-			EnvVar: "DRONE_YAML_VERIFIED",
-		},
-		cli.BoolFlag{
-			Name:   "yaml.signed",
-			Usage:  "build yaml is signed",
-			EnvVar: "DRONE_YAML_SIGNED",
-		},
-
-		//
-		// prev build args
-		//
-
-		cli.IntFlag{
-			Name:   "prev.build.number",
-			Usage:  "previous build number",
-			EnvVar: "DRONE_PREV_BUILD_NUMBER",
-		},
-		cli.StringFlag{
-			Name:   "prev.build.status",
-			Usage:  "previous build status",
-			EnvVar: "DRONE_PREV_BUILD_STATUS",
-		},
-		cli.StringFlag{
-			Name:   "prev.commit.sha",
-			Usage:  "previous build sha",
-			EnvVar: "DRONE_PREV_COMMIT_SHA",
-		},
-	}
-
-	app.Run(os.Args)
+func basicAuth(username, password string) string {
+    auth := username + ":" + password
+    return base64.StdEncoding.EncodeToString([]byte(auth))
 }
+    
+    
+func main() {
+  
+    PLUGIN_PROJECT_API := os.Getenv("PLUGIN_PROJECT_API")
+    PLUGIN_DEPLOY_IMAGE := os.Getenv("PLUGIN_DEPLOY_IMAGE")
+    RANCHER_ACCESS_KEY := os.Getenv("RANCHER_ACCESS_KEY")
+    RANCHER_SECRET_KEY := os.Getenv("RANCHER_SECRET_KEY")
+    var client http.Client
 
-func run(c *cli.Context) {
-	plugin := Plugin{
-		Repo: Repo{
-			Owner:   c.String("repo.owner"),
-			Name:    c.String("repo.name"),
-			Link:    c.String("repo.link"),
-			Avatar:  c.String("repo.avatar"),
-			Branch:  c.String("repo.branch"),
-			Private: c.Bool("repo.private"),
-			Trusted: c.Bool("repo.trusted"),
-		},
-		Build: Build{
-			Number:   c.Int("build.number"),
-			Event:    c.String("build.event"),
-			Status:   c.String("build.status"),
-			Deploy:   c.String("build.deploy"),
-			Created:  int64(c.Int("build.created")),
-			Started:  int64(c.Int("build.started")),
-			Finished: int64(c.Int("build.finished")),
-			Link:     c.String("build.link"),
-		},
-		Commit: Commit{
-			Remote:  c.String("remote.url"),
-			Sha:     c.String("commit.sha"),
-			Ref:     c.String("commit.sha"),
-			Link:    c.String("commit.link"),
-			Branch:  c.String("commit.branch"),
-			Message: c.String("commit.message"),
-			Author: Author{
-				Name:   c.String("commit.author.name"),
-				Email:  c.String("commit.author.email"),
-				Avatar: c.String("commit.author.avatar"),
-			},
-		},
-		Config: Config{
-		// plugin-specific parameters
-		},
-	}
+    req, err := http.NewRequest("GET", PLUGIN_PROJECT_API, nil)
+    req.SetBasicAuth(RANCHER_ACCESS_KEY, RANCHER_SECRET_KEY)
 
-	if err := plugin.Exec(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    } 
+
+    res, err2 := client.Do(req)
+  
+	if err2 != nil {
+        log.Fatal(err2)
+        os.Exit(1)
+    }    
+
+	defer res.Body.Close()
+    apiResponse, err := ioutil.ReadAll(res.Body)
+    var apiResponseStr string = string(apiResponse[:])
+    if s.Contains(apiResponseStr, "error") {
+        fmt.Printf("%s", apiResponse)
+        os.Exit(1)
+    }
+
+    value2, _ := sjson.Set(apiResponseStr, `containers.0.image`, PLUGIN_DEPLOY_IMAGE)
+    fmt.Println(value2)
+    
+    req3, err3 := http.NewRequest("PUT", PLUGIN_PROJECT_API, s.NewReader(value2))
+    req3.SetBasicAuth(RANCHER_ACCESS_KEY, RANCHER_SECRET_KEY)
+
+    if err3 != nil {
+        log.Fatal(err3)
+        os.Exit(1)
+    } 
+
+    res4, err4 := client.Do(req3)
+  
+	if err4 != nil {
+        log.Fatal(err4)
+        os.Exit(1)
+    }    
+
+    defer res4.Body.Close()
+    apiResponse2, err2 := ioutil.ReadAll(res4.Body)
+    var apiResponseStr2 string = string(apiResponse2[:])
+    if s.Contains(apiResponseStr2, "error") {
+        fmt.Printf("%s", apiResponse2)
+        os.Exit(1)
+    }
 }
