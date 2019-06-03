453b98fea07d0151479076393099e12b2e492203
hazel <hazel.shen@linecorp.com>
2019-06-03 23:18:47 +0800

    :bulb: Refactored code, prettier JSON response, and added readme.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 #Rancher plugin go version
+
+This plugin is for solving JavaScript plugin which doesn't failed event if the drone workload isn't updated.
+
+##How to test
+
+```
+export PLUGIN_PROJECT_API = ${Your Rancher workload API}
+export PLUGIN_DEPLOY_IMAGE = ${Your docker image url}
+export PLUGIN_ACCESS_KEY = ${Your Rancher API access key}
+export PLUGIN_SECRET_KEY = ${Your Rancher API secret key}
+
+go main.go
+```
+
+##HOW to use
+
+You can use this way:
+
+pipeline:
+deploy-on-rancher:
+    image: hazel910159/drone-deploy-plugin-go:v8
+    project_api: ${Your Rancher workload API}
+    access_key: ${Your Rancher API access key}
+    secret_key: ${Your Rancher API secret key}
+    deploy_image: ${Your docker image url}
+    when:
+      event: [push]
+      branch: [beta, master]
--- main.go
+++ main.go
@@ -1,75 +1,63 @@
 package main
 
 import (
-    "os"
 	"fmt"
 	"io/ioutil"
 	"log"
-    "net/http"
-    "encoding/base64"
-    "github.com/tidwall/sjson"
-    s "strings"
+	"net/http"
+	"os"
+	s "strings"
+
+	"github.com/tidwall/pretty"
+	"github.com/tidwall/sjson"
 )
-func basicAuth(username, password string) string {
-    auth := username + ":" + password
-    return base64.StdEncoding.EncodeToString([]byte(auth))
-}
-    
-    
+
 func main() {
-  
-    PLUGIN_PROJECT_API := os.Getenv("PLUGIN_PROJECT_API")
-    PLUGIN_DEPLOY_IMAGE := os.Getenv("PLUGIN_DEPLOY_IMAGE")
-    RANCHER_ACCESS_KEY := os.Getenv("RANCHER_ACCESS_KEY")
-    RANCHER_SECRET_KEY := os.Getenv("RANCHER_SECRET_KEY")
-    var client http.Client
 
-    req, err := http.NewRequest("GET", PLUGIN_PROJECT_API, nil)
-    req.SetBasicAuth(RANCHER_ACCESS_KEY, RANCHER_SECRET_KEY)
+	pluginProjectAPI := os.Getenv(`PLUGIN_PROJECT_API`)
+	pluginDeployImage := os.Getenv(`PLUGIN_DEPLOY_IMAGE`)
+	rancherAccessKey := os.Getenv(`PLUGIN_ACCESS_KEY`)
+	rancherSecretKey := os.Getenv(`PLUGIN_SECRET_KEY`)
 
-    if err != nil {
-        log.Fatal(err)
-        os.Exit(1)
-    } 
+	var client http.Client
 
-    res, err2 := client.Do(req)
-  
-	if err2 != nil {
-        log.Fatal(err2)
-        os.Exit(1)
-    }    
+	req, _ := http.NewRequest("GET", pluginProjectAPI, nil)
+	req.SetBasicAuth(rancherAccessKey, rancherSecretKey)
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
 	defer res.Body.Close()
-    apiResponse, err := ioutil.ReadAll(res.Body)
-    var apiResponseStr string = string(apiResponse[:])
-    if s.Contains(apiResponseStr, "error") {
-        fmt.Printf("%s", apiResponse)
-        os.Exit(1)
-    }
 
-    value2, _ := sjson.Set(apiResponseStr, `containers.0.image`, PLUGIN_DEPLOY_IMAGE)
-    fmt.Println(value2)
-    
-    req3, err3 := http.NewRequest("PUT", PLUGIN_PROJECT_API, s.NewReader(value2))
-    req3.SetBasicAuth(RANCHER_ACCESS_KEY, RANCHER_SECRET_KEY)
+	projectInfo, _ := ioutil.ReadAll(res.Body)
+	projectInfoStr := string(projectInfo[:])
 
-    if err3 != nil {
-        log.Fatal(err3)
-        os.Exit(1)
-    } 
+	if s.Contains(projectInfoStr, "error") {
+		os.Exit(1)
+	}
 
-    res4, err4 := client.Do(req3)
-  
-	if err4 != nil {
-        log.Fatal(err4)
-        os.Exit(1)
-    }    
+	updatedProjectInfo, _ := sjson.Set(projectInfoStr, `containers.0.image`, pluginDeployImage)
 
-    defer res4.Body.Close()
-    apiResponse2, err2 := ioutil.ReadAll(res4.Body)
-    var apiResponseStr2 string = string(apiResponse2[:])
-    if s.Contains(apiResponseStr2, "error") {
-        fmt.Printf("%s", apiResponse2)
-        os.Exit(1)
-    }
+	req, _ = http.NewRequest("PUT", pluginProjectAPI, s.NewReader(updatedProjectInfo))
+	req.SetBasicAuth(rancherAccessKey, rancherSecretKey)
+
+	res, err = client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+	updatedResult, _ := ioutil.ReadAll(res.Body)
+	updatedResultStr := string(updatedResult)
+	updatedResultJSON := pretty.Pretty(updatedResult)
+
+	fmt.Printf("%s", updatedResultJSON)
+	if s.Contains(updatedResultStr, "error") {
+		os.Exit(1)
+	}
 }
